f72755b51f5b0134ebb25e1fedc45f570f656394
Vleo <jassim.vleo@gmail.com>
2024-09-09 08:31:21 +0300

    remove useEffect from layout of home page

--- app/layout.js
+++ app/layout.js
@@ -9,14 +9,6 @@ import { useEffect } from 'react';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
-  useEffect(()=>{
-    if (typeof window !== "undefined") {
-      // This code will only run on the client side
-      const cookiesUser = JSON.parse(Cookies.get('user'));
-      console.log('User from cookies:', cookiesUser);
-      setUser(cookiesUser);
-    }
-  },[])
  return (
     <html lang="en">
       <body className={inter.className} >
